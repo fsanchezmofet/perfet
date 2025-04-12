73d1d80f053d9956b17fb04fc8257e1fa4a7cf2f
han057 <fredyhernan057@gmail.com>
2025-04-12 12:45:58 -0500

    Instalación de laravel

---
+++ resources/js/components/AppLogoIcon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+
+defineOptions({
+    inheritAttrs: false,
+});
+
+interface Props {
+    className?: HTMLAttributes['class'];
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <img src="https://fet.edu.co/images/logo.png">
+</template>
